7fd768ef6e1ed688e7de3d619e486ee168ee5bdb
hip06 <khongpikten21@gmail.com>
2022-10-19 19:46:31 +0700

    v4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
-
   const [work, setWork] = useState('')
-  console.log(work)
-
-
+  const [todos, setTodos] = useState([])
+  const handleAdd = () => {
+    if (todos?.some(item => item.id === work?.replace(/\s/g, ''))) {
+      toast.warn('Công việc đã được thêm vào trước đó')
+    } else {
+      setTodos(prev => [...prev, { id: work?.replace(/\s/g, ''), job: work }])
+      setWork('')
+    }
+  }
+  const handleDeleteJob = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id))
+  }
   return (
-    <div className="flex gap-8 h-screen items-center border border-red-500 justify-center">
-      <input
-        type="text"
-        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
-        value={work}
-        onChange={e => setWork(e.target.value)}
+    <>
+      <div className="flex flex-col gap-8 h-screen items-center border border-red-500 justify-center">
+        <div className='flex gap-8'>
+          <input
+            type="text"
+            className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+            value={work}
+            onChange={e => setWork(e.target.value)}
+          />
+          <button
+            type="button"
+            className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
+        <div>
+          <h3 className='font-bold text-xl'>Content:</h3>
+          <ul>
+            {todos?.map((item) => {
+              return (
+                <li key={item.id} className='flex gap-10 items-center'>
+                  <span className='my-2'>{item.job}</span>
+                  <span onClick={() => handleDeleteJob(item.id)} className='my-2 cursor-pointer p-2'>X</span>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
-      <button
-        type="button"
-        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
-      >
-        Add
-      </button>
-
-    </div>
+    </>
   );
 }
 
