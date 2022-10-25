a56ed65b03af56d38d88bea4efa65f872a0b3eaf
hip06 <khongpikten21@gmail.com>
2022-10-25 18:19:01 +0700

    done slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { getArrSlider } from '../ultis/fn'
 
@@ -8,17 +8,31 @@ const Slider = () => {
 
     useEffect(() => {
         const sliderEls = document.getElementsByClassName('slider-item')
-        let min = 0
-        let max = 2
+        let min = 1
+        let max = 3
         const intervalId = setInterval(() => {
             const list = getArrSlider(min, max, sliderEls.length - 1)
+
             for (let i = 0; i < sliderEls.length; i++) {
+                sliderEls[i].classList.remove('animate-slide-right', 'order-last', 'z-10')
+                sliderEls[i].classList.remove('animate-slide-left', 'order-first', 'z-0')
+                sliderEls[i].classList.remove('animate-slide-left2', 'order-2', 'z-0')
+
                 if (list.some(item => item === i)) {
                     sliderEls[i].style.cssText = `display: block`
                 } else {
                     sliderEls[i].style.cssText = `display: none`
                 }
             }
+            list.forEach((item) => {
+                if (item === max) {
+                    sliderEls[max].classList.add('animate-slide-right', 'order-last', 'z-10')
+                } else if (item === min) {
+                    sliderEls[min].classList.add('animate-slide-left', 'order-first', 'z-0')
+                } else {
+                    sliderEls[item].classList.add('animate-slide-left2', 'order-2', 'z-0')
+                }
+            })
             if (min === sliderEls.length - 1) {
                 min = 0
             } else {
@@ -29,8 +43,7 @@ const Slider = () => {
             } else {
                 max += 1
             }
-            console.log(list)
-        }, 1000)
+        }, 3000)
         return () => {
             intervalId && clearInterval(intervalId)
         }
@@ -38,11 +51,11 @@ const Slider = () => {
 
     return (
         <div className='flex gap-4 w-full overflow-hidden px-[59px] pt-8'>
-            {banner?.map(item => (
+            {banner?.map((item, index) => (
                 <img
                     key={item.encodeId}
                     src={item.banner}
-                    className='slider-item flex-1 object-contain w-1/3 rounded-lg'
+                    className={`slider-item flex-1 object-contain w-1/3 rounded-lg ${index <= 2 ? 'block' : 'hidden'}`}
                 />
             ))}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,68 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scale-up-center': {
+          '0%': {
+            '-webkit-transform': 'scale(0.5)',
+            transform: 'scale(0.5)'
+          },
+          '100%': {
+            '-webkit-transform': 'scale(1)',
+            transform: 'scale(1)'
+          }
+        },
+        'slide-right': {
+          '0%': {
+            '-webkit-transform': 'translateX(-500px)',
+            transform: 'translateX(-500px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            transform: 'translateX(0)'
+          }
+        },
+        'slide-left': {
+          '0%': {
+            '-webkit-transform': 'translateX(300px)',
+            transform: 'translateX(300px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            transform: 'translateX(0)'
+          }
+        },
+        'dropdown': {
+          '0%': {
+            transform: 'translateY(-20px)',
+            opacity: 0
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1
+          }
+        },
+        'slide-left2': {
+          '0%': {
+            '-webkit-transform': 'translateX(300px)',
+            transform: 'translateX(300px)'
+          },
+          '100%': {
+            '-webkit-transform': 'translateX(0)',
+            transform: 'translateX(0)'
+          }
+        },
+      },
+
+      animation: {
+        'scale-up-center': 'scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+        'slide-right': 'slide-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'slide-left': 'slide-left2 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'slide-left2': 'slide-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'dropdown': 'dropdown 0.5s ease both',
+      },
+    },
   },
   plugins: [],
 }
