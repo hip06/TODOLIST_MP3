fa6eeaf29f31e1dbbdec450c3eeb98d6b4df895c
hip06 <khongpikten21@gmail.com>
2022-11-16 08:34:02 +0700

    chart

--- src/components/ChartSection.js
+++ src/components/ChartSection.js
@@ -11,19 +11,26 @@ const ChartSection = () => {
     const options = {
         responsive: true,
         pointRadius: 0,
-        aspectRatio: 5,
+        maintainAspectRatio: false,
         scales: {
             y: {
                 ticks: { display: false },
-                grid: { borderDash: [4, 4], color: 'rgba(255,255,255,0.5)' }
+                grid: { color: 'rgba(255,255,255,0.2)', drawTicks: false },
+                min: chart?.minScore,
+                max: chart?.maxScore,
+                border: { dash: [3, 4] }
             },
             x: {
                 ticks: { color: 'white' },
-                grid: { color: 'transparent' }
+                grid: { color: 'transparent' },
             }
         },
         plugins: {
             legend: false
+        },
+        hover: {
+            mode: 'dataset',
+            intersect: false
         }
     }
     // console.log({ chart, rank });
@@ -34,23 +41,29 @@ const ChartSection = () => {
             for (let i = 0; i < 3; i++) {
                 datasets.push({
                     data: chart?.items[Object.keys(chart?.items)[i]]?.filter(item => +item.hour % 2 === 0)?.map(item => item.counter),
-                    borderColor: i === 0 ? 'blue' : i === 1 ? 'yellow' : 'red',
-                    tension: 0.2,
-                    borderWidth: 2
+                    borderColor: i === 0 ? '#4a90e2' : i === 1 ? '#50e3c2' : '#e35050',
+                    tension: 0.3,
+                    borderWidth: 2,
+                    pointHoverRadius: 5,
+                    pointBackgroundColor: 'white',
+                    pointHitRadius: 5,
+                    pointBorderColor: i === 0 ? '#4a90e2' : i === 1 ? '#50e3c2' : '#e35050',
+                    animation: false,
+                    pointHoverBorderWidth: 5
                 })
             }
             setData({ labels, datasets })
         }
     }, [chart])
     return (
-        <div className='px-[59px] mt-12 relative'>
-            <img src={bgChart} alt="bg-chart" className='w-full object-cover rounded-md' />
+        <div className='px-[59px] mt-12 relative max-h-[350px]'>
+            <img src={bgChart} alt="bg-chart" className='w-full max-h-[350px] object-cover rounded-md' />
             <div className='absolute top-0 z-10 left-[59px] bg-[rgba(77,34,104,0.9)] right-[59px] bottom-0'></div>
-            <div className='absolute top-0 z-20 left-[59px] right-[59px] bottom-0 p-5'>
+            <div className='absolute top-0 z-20 left-[59px] right-[59px] bottom-0 p-5 flex flex-col'>
                 <h3 className='text-2xl text-white font-bold'>#zingchart</h3>
                 <div className='flex gap-4 h-full'>
-                    <div className='flex-4 border border-white'>rank</div>
-                    <div className='flex-6 h-ful'>
+                    <div className='flex-3 border border-white'>rank</div>
+                    <div className='flex-7 h-full'>
                         {data && <Line data={data} options={options} />}
                     </div>
                 </div>
